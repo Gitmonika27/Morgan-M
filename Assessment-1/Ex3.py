d27d4382e43a6baab9beba00fe310f2fe8b123ae
Gitmonika27 <97837743+Gitmonika27@users.noreply.github.com>
2022-01-18 16:52:52 +0530

    Ex3. Py

--- Assessment-1/Ex3.py
+++ Assessment-1/Ex3.py
@@ -1,10 +1,8 @@
-def factorial(num):
-    if((num == 0) or (num == 1)):
-        return 1
+n = int(input("Enter value") 
+def factorial(n):
+    if n==1:
+        return n
     else:
-         return num * factorial(num - 1)
+        return n*factorial(n-1)
+Print("Answer is", factorial(n))
 
-number = int(input(" Please enter any Number to find factorial : "))
-
-fact = factorial(number)
-print("The factorial of %d  = %d" %(number, fact))
