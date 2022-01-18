77e33313e0aafae51d217d485ff7aa44d7143548
Gitmonika27 <97837743+Gitmonika27@users.noreply.github.com>
2022-01-18 19:06:05 +0530

    Ex6.py

--- Assessment-1/Ex6.py
+++ Assessment-1/Ex6.py
@@ -1,5 +1,3 @@
-string=[]
-string = [str(x) for x in input("Enter values\n").split(',')]
-sorted_value=string
-separator=","
-print(separator.join(sorted(sorted_value)))
+A = str(input("Enter the words with comma to sort:"))
+B = A.split(',')
+Print(sorted(B))
