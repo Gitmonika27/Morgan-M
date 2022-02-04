ddc430bccb8b2d21cee0af60b643a666c2c8bd05
Gitmonika27 <97837743+Gitmonika27@users.noreply.github.com>
2022-02-04 23:26:00 +0530

    Update Ex2Assessment1.py

--- Assessment 1/Ex2Assessment1.py
+++ Assessment 1/Ex2Assessment1.py
@@ -1,7 +1,7 @@
 a= 2000
 b= 3200
+strings=[]
 for i in range(a,b+1):
-
-   if(i%7==0 and i%5!=0):
-  strings.append(str(i))
+    if(i%7==0 and i%5!=0):
+        strings.append(str(i))
 print(', '.join(strings))
