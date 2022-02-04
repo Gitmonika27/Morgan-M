f139727ae335eee6a561cb10857a635591ef118a
Gitmonika27 <97837743+Gitmonika27@users.noreply.github.com>
2022-02-04 23:10:47 +0530

    Delete Assessment-1 directory

--- Assessment-1/EX9.py
+++
@@ -1,7 +0,0 @@
-dict={}
-dict=input("Enter the dict value:")
-Key=input("Enter the key:") 
-if key in dict:
- Print("Present")
-Else:
- Print("Not present") 
--- Assessment-1/Ex3.py
+++
@@ -1,8 +0,0 @@
-n = int(input("Enter value for factorial:"))
-def factorial(n):
-    if n==1:
-        return n
-    else:
-        return n*factorial(n-1)
-Print("Answer is", factorial(n))
-
