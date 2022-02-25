7400dc136607440ed18f3126949f387c25616146
Gitmonika27 <97837743+Gitmonika27@users.noreply.github.com>
2022-02-25 13:08:50 +0530

    Delete Assessment-4 directory

--- Assessment-4/Ex5-Assessment4.py
+++
@@ -1,8 +0,0 @@
-import requests
-import pandas as pd
-
-x = requests.get('https://jsonplaceholder.typicode.com/todos')
-print(x.status_code) #To print status code
-
-df=pd.read_json('https://jsonplaceholder.typicode.com/todos')
-print(df.head(5)) #To print data of 5 rows
